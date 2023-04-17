ec6ef2986869927f36e3b29dcaf6772d29face35
paveltovchigrechko <pavel.tovchigrecko.1988@gmail.com>
2023-04-17 18:21:33 +0600

    Moved packages for English into en

--- en/en.go
+++ en/en.go
@@ -1,7 +1,9 @@
-// Package dcr provides functions to calculate the Dale–Chall readability (DCR) formula for texts readability.
-//
-// See https://en.wikipedia.org/wiki/Dale%E2%80%93Chall_readability_formula for the details.
-package dcr
+// Package `en`` provides functions and types to calculate the readability for English texts.
+
+// 1. Automated readability index (ARI) (https://en.wikipedia.org/wiki/Automated_readability_index)
+// 2. Coleman–Liau index (CLI) (https://en.wikipedia.org/wiki/Coleman%E2%80%93Liau_index)
+// 3. Dale–Chall readability (DCR) formula (https://en.wikipedia.org/wiki/Dale%E2%80%93Chall_readability_formula)
+package en
 
 import (
 	"errors"
@@ -16,9 +18,133 @@ const (
 	ADJUSTED_SCORE       = 3.6365
 )
 
-// CalculateDCR accepts a non-empty string and returns the Dale–Chall readability (DCR) formula for it. The string must contain at least one word (a number is considered a word, for example `18.` is valid string) and at least one sentence.
+// ====== Types ======
+
+// AriResult represents the minimal age and grade to be able to read the text according automated readability index calculation.
+type AriResult struct {
+	// score      int8
+	age        string
+	gradeLevel string
+}
+
+// ariTable maps the ARI score to AriResult.
+var ariTable = map[int]AriResult{
+	1: {
+		"5-6",
+		"Kindengarden",
+	},
+	2: {
+		"6-7",
+		"First Grade",
+	},
+	3: {
+		"7-8",
+		"Second Grade",
+	},
+	4: {
+		"8-9",
+		"Third Grade",
+	},
+	5: {
+		"9-10",
+		"Forth Grade",
+	},
+	6: {
+		"10-11",
+		"Fifth Grade",
+	},
+	7: {
+		"11-12",
+		"Sixth Grade",
+	},
+	8: {
+		"12-13",
+		"Seventh Grade",
+	},
+	9: {
+		"13-14",
+		"Eighth Grade",
+	},
+	10: {
+		"14-15",
+		"Ninth Grade",
+	},
+	11: {
+		"15-16",
+		"Tenth Grade",
+	},
+	12: {
+		"16-17",
+		"Eleventh Grade",
+	},
+	13: {
+		"17-18",
+		"Twelfth Grade",
+	},
+	14: {
+		"18-22",
+		"College student",
+	},
+}
+
+// ====== Functions ======
+
+// CalcAri accepts a non-empty string and returns the automated readability index (ARI) of it. The string has to have at least one word and at least one sentence (ended with `.`, `?`, `!`, or `...`)
+// The result is always rounded up to the nearest whole number.
+func CalcAri(s string) (int, error) {
+	if len(s) == 0 {
+		return 0, errors.New("Empty string.")
+	}
+	characters := float64(stats.CountCharacters(s))
+	words := float64(stats.CountWords(s))
+	sentences := float64(stats.CountSentences(s))
+
+	if words == 0 || sentences == 0 {
+		return 0, errors.New("No words or sentences were parsed. Cannot calculate automated readability index (ARI).")
+	}
+
+	ariFloat := 4.71*(characters/words) + 0.5*(words/sentences) - 21.43
+	// fmt.Println("Rough ARI:", ariFloat)
+	score := int(math.Ceil(ariFloat))
+	return score, nil
+}
+
+// ConvertARItoGrades accepts an ARI score as integer and returns the mapped to the score age and grade as strings.
+//
+// If no structure found, returns {"Unknown", "Unknown"}.
+func ConvertAriToGrades(score int) (age, grade string) {
+	if score > 14 {
+		return "22+", "Professor level"
+	}
+	if _, ok := ariTable[score]; !ok {
+		return "Unknown", "Unknown"
+	}
+	return ariTable[score].age, ariTable[score].gradeLevel
+}
+
+// CalcCli accepts a non-empty string and returns the Coleman–Liau index (CLI) for it. The string must contain at least one word (a number is considered a word, for example `18.` is valid string) and at least one sentence.
+// The calculated CLI is rounded to the first decimal point.
+func CalcCli(s string) (float64, error) {
+	if len(s) == 0 {
+		return 0, errors.New("Empty string.")
+	}
+
+	characters := float64(stats.CountCharacters(s))
+	words := float64(stats.CountWords(s))
+	sentences := float64(stats.CountSentences(s))
+
+	if words == 0 {
+		return 0, errors.New("No words were parsed. Cannot calculate Coleman–Liau index (CLI).")
+	}
+
+	cli := 5.88*(characters/words) - 29.6*(sentences/words) - 15.8
+	cli = math.Round(cli*10) / 10
+	return cli, nil
+}
+
+// CalcDCR accepts a non-empty string and returns the Dale–Chall readability (DCR) formula for it. The string must contain at least one word (a number is considered a word, for example `18.` is a valid string) and at least one sentence.
 // The calculated DCR is rounded to the second decimal point.
-func CalculateDCR(s string) (float64, error) {
+func CalcDCR(s string) (float64, error) {
 	if len(s) == 0 {
 		return 0, errors.New("Empty string.")
 	}
